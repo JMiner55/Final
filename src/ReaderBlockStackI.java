6aa9537682cbe67644ba913f415f2fc7ebbd5c33
Jason Miner <jminer@carroll.edu>
2021-12-14 15:19:06 -0700

    added ReaderBlockStackI.java interface

---
+++ src/ReaderBlockStackI.java
@@ -0,0 +1,17 @@
+
+public interface ReaderBlockStackI
+{
+	/**
+	 * Adds entry to the stack.
+	 *
+	 * @param item – Item to add to the front of FIFO
+	 */
+	void addItem(String item);
+
+	/**
+	 * Remove newest item in the stack. Will block until at least one item exists in
+	 * stack.
+	 *
+	 */
+	String remove();
+}
